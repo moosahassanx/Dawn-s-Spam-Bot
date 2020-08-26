0811a105d46a6bc5c231e5e4b6538f087393de2d
Moosa Hassan <moosa7021@gmail.com>
2020-08-26 12:31:21 +1000

    changed some settings

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 3000;
+            this.timer1.Interval = 150;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button1
